ca62254c06953bd53a58c545f55c74934e347ed4
DariusL <d.lapunas@gmail.com>
2013-09-30 19:16:50 +0300

    tikslesnis (not really)

--- main.m
+++ main.m
@@ -1,10 +1,11 @@
+clear;
 x = 0:0.01:10;
 y = funkcija(x);
 figure(1);
 plot(x, y, 'LineWidth', 2);
 grid on;
-rg = grubus([1 -31 215 -5 -2016 -1764]) / 2;
-x = linspace(-rg - 2, rg + 2, 1000);
+rg = tikslesnis([1 -31 215 -5 -2016 -1764]);
+x = linspace(rg(1) - 2, rg(2) + 2, 1000);
 y = daugianaris(x);
 figure(2);
 plot(x, y, 'LineWidth', 2);
---
+++ tikslesnis.m
@@ -0,0 +1,43 @@
+function rVec = tikslesnis(koef)
+    rVec = [-grubus(koef), grubus(koef)];
+    B = getB(koef);
+    k = getK(koef);
+    R = 1 + (B / koef(1))^(1 / k);
+    if(R < rVec(2))
+        rVec(2) = R;
+    end
+    koef = magic(koef);
+    B = getB(koef);
+    k = getK(koef);
+    R = 1 + (B / koef(1))^(1 / k);
+    if(-R > rVec(1))
+        rVec(1) = -R;
+    end
+end
+
+function B = getB(koef)
+    B = realmin('double');
+    for i = 2:length(koef)
+        if(koef(i) < 0 && abs(koef(i)) > B)
+            B = abs(koef(i));
+        end
+    end
+end
+
+function k = getK(koef)
+    for i = 2:length(koef)
+        if (koef(i) < 0)
+            k = -1 + i;
+            return;
+        end
+    end
+end
+
+function ret = magic(koef)
+    ret = koef;
+    for i = 1:length(koef)
+        if(mod(i, 2) == 0)
+            ret(i) = -ret(i);
+        end
+    end
+end
