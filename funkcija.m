5d41dc0e4a8b9cfee08c334c9cbaea54bdd2fd5d
DariusL <d.lapunas@gmail.com>
2013-10-01 18:22:16 +0300

    gasghahasdh

--- funkcija.m
+++
@@ -1,3 +0,0 @@
-function y = funkcija(x)
-    y = sin(x .* 2)./((x + 1).^2);
-end
--- main.m
+++ main.m
@@ -1,16 +1,5 @@
 clear;
-x = 0:0.01:10;
-y = funkcija(x);
-figure(1);
-plot(x, y, 'LineWidth', 2);
-grid on;
 koef = [1 -31 215 -5 -2016 -1764];
 rg = tikslesnis(koef);
-x = linspace(rg(1) - 2, rg(2) + 2, 1000);
-f = @(x)(x^5 - 31 * x^4 + 215 * x^3 - 5 * x^2 - 2016 * x - 1764);
-roots = skenavimas(f, rg(1)-2, rg(2)+2, 1, 0.01);
-y = daugianaris(x);
-figure(2);
-plot(x, y, 'LineWidth', 2);
-hold on;
-grid on;
+d = @(x)(x^5 - 31 * x^4 + 215 * x^3 - 5 * x^2 - 2016 * x - 1764);
+droots = skenavimas(d, rg(1)-2, rg(2)+2, 1, 0.01);
