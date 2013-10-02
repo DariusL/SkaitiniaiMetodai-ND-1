e393b83f8a2caf5c152bd97d925c22b22eb57607
unknown <darlap1@IF312-022.sc.ad.ktu.lt>
2013-10-02 15:47:10 +0300

    gfagasg

--- skenavimas.m
+++ skenavimas.m
@@ -2,18 +2,19 @@ function vec = skenavimas(func, pradzia, pabaiga, zingsniai, tikslumas)
     vec = [];
     zingsnis = (pabaiga - pradzia)/zingsniai;
     for i = pradzia:zingsnis:pabaiga-zingsnis
+        plot(i, 0, '^b');
         if(func(i) * func(i + zingsnis) < 0)
             if(abs(func(i+zingsnis/2)) <= tikslumas)
                 vec = [vec i+zingsnis/2];
                 plot(vec, 0 , '*k');
             else
                 vec = [vec skenavimas(func, i, i + zingsnis, 10, tikslumas)];
-                plot(vec, 0 , '*k');
+                plot(vec, 0*vec, '*k');
             end
         else
             if(func(i) == 0)
                 vec = [vec i];
-                plot(vec, 0 , '*k');
+                plot(vec, 0*vec, '*k');
             end
         end
     end
